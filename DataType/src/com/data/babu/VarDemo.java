45cf85c51f52c46c0396c3b23a0379bccd2d894d
Nasrullah Bari <nasrullah.it.qa@gmail.com>
2022-01-24 22:43:23 -0500

    check

---
+++ DataType/src/com/data/babu/VarDemo.java
@@ -0,0 +1,18 @@
+package com.data.babu;
+
+public class VarDemo{
+
+
+    public static void main(String[] args) {
+
+        var b  = 1000000000;
+        var name = "Nasrullah";
+        var address = "611 Eggert Road, Buffalo, NY 14215";
+
+        System.out.println(b);
+        System.out.println(name);
+        System.out.println(address);
+
+    }
+
+}
